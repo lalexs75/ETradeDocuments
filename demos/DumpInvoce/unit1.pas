cebd57b14fde273915cd4e7a3a0265828d856dfe
lalexs75 <alexs75@yandex.ru>
2019-02-26 09:31:52 +0300

    Тестовое чтение СФ

--- demos/DumpInvoce/unit1.pas
+++ demos/DumpInvoce/unit1.pas
@@ -7,8 +7,8 @@ interface
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls,
   ETradeDoc, DividerBevel,
-  InvoceExchangeFile, InvoceDocument, InvoiceItem, TransferInfo, Signer,
-  AdditionalInfo;
+  AdressInfo, InvoceExchangeFile, InvoceDocument, InvoiceItem, TransferInfo, Signer,
+  AdditionalInfo, OrganizationInfo;
 
 type
 
@@ -62,6 +62,30 @@ type
     procedure DumpTextInfo(P:TTextInfo; APrefix: string);
     procedure DumpShipmentBaseList(P:TShipmentBaseList; APrefix: string);
     procedure DumpShipmentBase(P:TShipmentBase; APrefix: string);
+    procedure DumpTransferEmployee(P:TTransferEmployee; APrefix: string);
+    procedure DumpSellerEmployee(P:TSellerEmployee; APrefix: string);
+    procedure DumpPerson(P:TPerson; APrefix: string);
+    procedure DumpOtherIssuer(P:TOtherIssuer; APrefix: string);
+    procedure DumpOtherIssuerEmployee(P:TOtherIssuerEmployee; APrefix: string);
+    procedure DumpPhysicalPersonTransfer(P:TPhysicalPersonTransfer; APrefix: string);
+    procedure DumpTransportationCargo(P:TTransportationCargo; APrefix: string);
+    procedure DumpWaybillList(P:TWaybillList; APrefix: string);
+    procedure DumpWaybill(P:TWaybill; APrefix: string);
+    procedure DumpOrganizationInfo(P:TOrganizationInfo; APrefix: string);
+    procedure DumpIdentificationInformation(P:TIdentificationInformation; APrefix: string);
+    procedure DumpIndividualEntrepreneurInformation(P:TIndividualEntrepreneurInformation; APrefix: string);
+    procedure DumpLegalEntityInformation(P:TLegalEntityInformation; APrefix: string);
+    procedure DumpForeignEntityInformation(P:TForeignEntityInformation; APrefix: string);
+    procedure DumpPhysicalPersonEntity(P:TPhysicalPersonEntity; APrefix: string);
+    procedure DumpAdress(P:TAdress; APrefix: string);
+    procedure DumpRussianAdress(P:TRussianAdress; APrefix: string);
+    procedure DumpAdressInfo(P:TAdressInfo; APrefix: string);
+    procedure DumpContactInfo(P:TContactInfo; APrefix: string);
+    procedure DumpBankDetails(P:TBankDetails; APrefix: string);
+    procedure DumpBankInfo(P:TBankInfo; APrefix: string);
+    procedure DumpCreatedThing(P:TCreatedThing; APrefix: string);
+    procedure DumpSigner(P:TSigner; APrefix: string);
+    procedure DumpLegalEntityEmployee(P:TLegalEntityEmployee; APrefix: string);
 
 
     procedure WriteLogI(S:string; AParams:array of const);
@@ -333,8 +357,12 @@ begin
 end;
 
 procedure TForm1.DumpSigners(P: TSignerList; APrefix: string);
+var
+  i: Integer;
 begin
-
+  if not Assigned(P) then Exit;
+  for i:=0 to P.Count-1 do
+    DumpSigner(P[i], Format(APrefix + '[%d]', [i]));
 end;
 
 procedure TForm1.DumpItemsTransferInfo(P: TItemsTransferInfo; APrefix: string);
@@ -347,9 +375,9 @@ begin
   WriteLogI(APrefix + 'DateBegin = %s', [P.DateBegin]);
   WriteLogI(APrefix + 'DateEnd = %s', [P.DateEnd]);
   DumpShipmentBaseList(P.TransferBase, APrefix + 'TransferBase');
-  //property TransferEmployee:TTransferEmployee read FTransferEmployee;
-  //property TransportationCargo:TTransportationCargo read FTransportationCargo;
-  //property CreatedThing:TCreatedThing read FCreatedThing;
+  DumpTransferEmployee(P.TransferEmployee, APrefix + 'TransferEmployee');
+  DumpTransportationCargo(P.TransportationCargo, APrefix + 'TransportationCargo');
+  DumpCreatedThing(P.CreatedThing, APrefix + 'CreatedThing');
 end;
 
 procedure TForm1.DumpAdditionalInfo3(P: TAdditionalInfo3; APrefix: string);
@@ -395,6 +423,257 @@ begin
   WriteLogI(APrefix + 'BaseDocumentDate = %s', [P.BaseDocumentDate]);
   WriteLogI(APrefix + 'BaseDocumentInfo = %s', [P.BaseDocumentInfo]);
   WriteLogI(APrefix + 'BaseDocumentID = %s', [P.BaseDocumentID]);
+end;
+
+procedure TForm1.DumpTransferEmployee(P: TTransferEmployee; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  DumpSellerEmployee(P.SellerEmployee, APrefix + 'SellerEmployee');
+  DumpOtherIssuer(P.OtherIssuer, APrefix + 'OtherIssuer');
+end;
+
+procedure TForm1.DumpSellerEmployee(P: TSellerEmployee; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Position = %s', [P.Position]);
+  WriteLogI(APrefix + 'OtherInformation = %s', [P.OtherInformation]);
+  WriteLogI(APrefix + 'PermisionPosition = %s', [P.PermisionPosition]);
+  DumpPerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TForm1.DumpPerson(P: TPerson; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Surname = %s', [P.Surname]);
+  WriteLogI(APrefix + 'FirstName = %s', [P.FirstName]);
+  WriteLogI(APrefix + 'Patronymic = %s', [P.Patronymic]);
+end;
+
+procedure TForm1.DumpOtherIssuer(P: TOtherIssuer; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  DumpOtherIssuerEmployee(P.OtherIssuerEmployee, APrefix + 'OtherIssuerEmployee');
+  DumpPhysicalPersonTransfer(P.PhysicalPersonTransfer, APrefix + 'PhysicalPersonTransfer');
+end;
+
+procedure TForm1.DumpOtherIssuerEmployee(P: TOtherIssuerEmployee;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Position = %s', [P.Position]);
+  WriteLogI(APrefix + 'OtherInfo = %s', [P.OtherInfo]);
+  WriteLogI(APrefix + 'OrganizationName = %s', [P.OrganizationName]);
+  WriteLogI(APrefix + 'TransferBase = %s', [P.TransferBase]);
+  WriteLogI(APrefix + 'PositionBase = %s', [P.PositionBase]);
+  DumpPerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TForm1.DumpPhysicalPersonTransfer(P: TPhysicalPersonTransfer;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'OtherInfo = %s', [P.OtherInfo]);
+  WriteLogI(APrefix + 'TransferBase = %s', [P.TransferBase]);
+  DumpPerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TForm1.DumpTransportationCargo(P: TTransportationCargo;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'TransferTextInfo = %s', [P.TransferTextInfo]);
+  DumpWaybillList(P.Waybills, APrefix + 'Waybills');
+  DumpOrganizationInfo(P.Carrier, APrefix + 'Carrier');
+end;
+
+procedure TForm1.DumpWaybillList(P: TWaybillList; APrefix: string);
+var
+  i: Integer;
+begin
+  if not Assigned(P) then Exit;
+  for i:=0 to P.Count-1 do
+    DumpWaybill(P[i], Format(APrefix + '[%d]', [i]));
+end;
+
+procedure TForm1.DumpWaybill(P: TWaybill; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'TransferDocumentNumber = %s', [P.TransferDocumentNumber]);
+  WriteLogI(APrefix + 'TransferDocumentDate = %s', [P.TransferDocumentDate]);
+end;
+
+procedure TForm1.DumpOrganizationInfo(P: TOrganizationInfo; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+
+  WriteLogI(APrefix + 'OKPO = %s', [P.OKPO]);
+  WriteLogI(APrefix + 'Department = %s', [P.Department]);
+  WriteLogI(APrefix + 'OrganizationOrPersonInfo = %s', [P.OrganizationOrPersonInfo]);
+  WriteLogI(APrefix + 'ShortName = %s', [P.ShortName]);
+  DumpIdentificationInformation(P.IdentificationInformation, APrefix + 'IdentificationInformation');
+  DumpAdress(P.Adress, APrefix + 'Adress');
+  DumpContactInfo(P.ContactInfo, APrefix + 'ContactInfo');
+  DumpBankDetails(P.BankDetails, APrefix + 'BankDetails');
+end;
+
+procedure TForm1.DumpIdentificationInformation(P: TIdentificationInformation;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  DumpIndividualEntrepreneurInformation(P.IndividualEntrepreneurInformation, APrefix + 'IndividualEntrepreneurInformation');
+  DumpLegalEntityInformation(P.LegalEntityInformation, APrefix + 'LegalEntityInformation');
+  DumpForeignEntityInformation(P.ForeignEntityInformation, APrefix + 'ForeignEntityInformation');
+  DumpPhysicalPersonEntity(P.PhysicalPerson, APrefix + 'PhysicalPerson');
+end;
+
+procedure TForm1.DumpIndividualEntrepreneurInformation(
+  P: TIndividualEntrepreneurInformation; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'INN = %s', [P.INN]);
+  WriteLogI(APrefix + 'INNDef = %s', [P.INNDef]);
+  WriteLogI(APrefix + 'IndividualEntityRegistrationCertificate = %s', [P.IndividualEntityRegistrationCertificate]);
+  WriteLogI(APrefix + 'OtherInfo = %s', [P.OtherInfo]);
+  DumpPerson(P.Person, APrefix + 'Person');
+
+end;
+
+procedure TForm1.DumpLegalEntityInformation(P: TLegalEntityInformation;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'FullName = %s', [P.FullName]);
+  WriteLogI(APrefix + 'INN = %s', [P.INN]);
+  WriteLogI(APrefix + 'INNDef = %s', [P.INNDef]);
+  WriteLogI(APrefix + 'KPP = %s', [P.KPP]);
+end;
+
+procedure TForm1.DumpForeignEntityInformation(P: TForeignEntityInformation;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'FullName', [P.FullName]);
+  WriteLogI(APrefix + 'Identifier', [P.Identifier]);
+  WriteLogI(APrefix + 'OtherInfo', [P.OtherInfo]);
+end;
+
+procedure TForm1.DumpPhysicalPersonEntity(P: TPhysicalPersonEntity;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+
+  WriteLogI(APrefix + 'CertificateStateRegistration', [P.CertificateStateRegistration]);
+  WriteLogI(APrefix + 'INN', [P.INN]);
+  WriteLogI(APrefix + 'OtherInfo', [P.OtherInfo]);
+  DumpPerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TForm1.DumpAdress(P: TAdress; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  DumpRussianAdress(P.RussianAdress, APrefix + 'RussianAdress');
+  DumpAdressInfo(P.AdressInfo, APrefix + 'AdressInfo');
+  WriteLogI(APrefix + 'CodeGAR', [P.CodeGAR]);
+end;
+
+procedure TForm1.DumpRussianAdress(P: TRussianAdress; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'ZipCode', [P.ZipCode]);
+  WriteLogI(APrefix + 'Region', [P.Region]);
+  WriteLogI(APrefix + 'Territory', [P.Territory]);
+  WriteLogI(APrefix + 'City', [P.City]);
+  WriteLogI(APrefix + 'Locality', [P.Locality]);
+  WriteLogI(APrefix + 'Street', [P.Street]);
+  WriteLogI(APrefix + 'Building', [P.Building]);
+  WriteLogI(APrefix + 'Block', [P.Block]);
+  WriteLogI(APrefix + 'Apartment', [P.Apartment]);
+end;
+
+procedure TForm1.DumpAdressInfo(P: TAdressInfo; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+
+  WriteLogI(APrefix + 'CountryCode', [P.CountryCode]);
+  WriteLogI(APrefix + 'Address', [P.Address]);
+end;
+
+procedure TForm1.DumpContactInfo(P: TContactInfo; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Phone', [P.Phone]);
+  WriteLogI(APrefix + 'EMail', [P.EMail]);
+end;
+
+procedure TForm1.DumpBankDetails(P: TBankDetails; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Account', [P.Account]);
+  DumpBankInfo(P.BankInfo, APrefix + 'BankInfo');
+end;
+
+procedure TForm1.DumpBankInfo(P: TBankInfo; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Name', [P.Name]);
+  WriteLogI(APrefix + 'BIK', [P.BIK]);
+  WriteLogI(APrefix + 'KorrAcount', [P.KorrAcount]);
+end;
+
+procedure TForm1.DumpCreatedThing(P: TCreatedThing; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'CreatedThingTransferDate', [P.CreatedThingTransferDate]);
+  WriteLogI(APrefix + 'CreatedThingInfo', [P.CreatedThingInfo]);
+end;
+
+procedure TForm1.DumpSigner(P: TSigner; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'SignerPowers', [P.SignerPowers]);
+  WriteLogI(APrefix + 'SignerStatus', [P.SignerStatus]);
+  WriteLogI(APrefix + 'SignerPowersBase', [P.SignerPowersBase]);
+  WriteLogI(APrefix + 'SignerOrgPowersBase', [P.SignerOrgPowersBase]);
+  DumpPhysicalPersonEntity(P.PhysicalPersonEntity, APrefix + 'PhysicalPersonEntity');
+  DumpIndividualEntrepreneurInformation(P.IndividualEntrepreneurInformation, APrefix + 'IndividualEntrepreneurInformation');
+  DumpLegalEntityEmployee(P.LegalEntityEmployee, APrefix + 'LegalEntityEmployee');
+end;
+
+procedure TForm1.DumpLegalEntityEmployee(P: TLegalEntityEmployee;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+
+  WriteLogI(APrefix + 'RegistrationInfo', [P.RegistrationInfo]);
+  WriteLogI(APrefix + 'Inn', [P.Inn]);
+  WriteLogI(APrefix + 'OrganizationName', [P.OrganizationName]);
+  WriteLogI(APrefix + 'Position', [P.Position]);
+  WriteLogI(APrefix + 'OtherInfo', [P.OtherInfo]);
+
+  DumpPerson(P.Person, APrefix + 'Person');
 end;
 
 procedure TForm1.WriteLogI(S: string; AParams: array of const);
