45a8947ce8bd716ae94a4f1396c3e83f84d32cf2
lalexs75 <alexs75@yandex.ru>
2019-02-26 16:49:27 +0300

    Доработка примера

--- demos/DumpInvoce/unit2.pas
+++ demos/DumpInvoce/unit2.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, StdCtrls, DividerBevel, ETradeDoc,
-  ClientExchangeFile, ExchangeDocument;
+  ClientExchangeFile, ExchangeDocument, ExchangeInformation;
 
 type
 
@@ -26,6 +26,18 @@ type
     procedure DumpSenderExchangeInformationEx(P:TSenderExchangeInformationEx; APrefix: string);
     procedure DumpExchangeDocument(P:TExchangeDocument; APrefix: string);
     procedure DumpExchangeFileIdentificatorSeller(P:TExchangeFileIdentificatorSeller; APrefix: string);
+    procedure DumpExchangeInformation(P:TExchangeInformation; APrefix: string);
+    procedure DumpAcceptanceInformation(P:TAcceptanceInformation; APrefix: string);
+    procedure DumpOperationCode(P:TOperationCode; APrefix: string);
+    procedure DumpCargoPerson(P:TCargoPerson; APrefix: string);
+    procedure DumpCustomerEmployee(P:TCustomerEmployee; APrefix: string);
+    procedure DumpExchangePerson(P:TExchangePerson; APrefix: string);
+    procedure DumpExchangeOtherIssuer(P:TExchangeOtherIssuer; APrefix: string);
+    procedure DumpAacceptanceLegalEntityEmployee(P:TAacceptanceLegalEntityEmployee; APrefix: string);
+    procedure DumpIndividualAcceptanceGoods(P:TIndividualAcceptanceGoods; APrefix: string);
+    procedure DumpAdditionalInfoId4(P:TAdditionalInfoId4; APrefix: string);
+    procedure DumpExchangeTextInfoList(P:TExchangeTextInfoList; APrefix: string);
+    procedure DumpExchangeTextInfo(P:TExchangeTextInfo; APrefix: string);
 
     procedure WriteLogI(S:string; AParams:array of const);
   public
@@ -91,7 +103,7 @@ begin
   WriteLogI(APrefix + 'DocumentCreator = %s', [P.DocumentCreator]);
   WriteLogI(APrefix + 'DocumentCreatorBase = %s', [P.DocumentCreatorBase]);
   DumpExchangeFileIdentificatorSeller(P.ExchangeFileIdentificatorSeller, APrefix + 'ExchangeFileIdentificatorSeller');
-  //property ExchangeInformation:TExchangeInformation read FExchangeInformation;
+  DumpExchangeInformation(P.ExchangeInformation, APrefix + 'ExchangeInformation');
   //property TreasuryInformation:TTreasuryInformation read FTreasuryInformation;
   //property ExchangeSigner:TExchangeSignerList read FSigner;
 end;
@@ -105,6 +117,133 @@ begin
   WriteLogI(APrefix + 'DateCreate = %s', [P.DateCreate]);
   WriteLogI(APrefix + 'TimeCreate = %s', [P.TimeCreate]);
   WriteLogI(APrefix + 'Sign = %s', [P.Sign]);
+end;
+
+procedure TExchInfoFrame.DumpExchangeInformation(P: TExchangeInformation;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'DocumentName = %s', [P.DocumentName]);
+  WriteLogI(APrefix + 'DocumentFunction = %s', [P.DocumentFunction]);
+  WriteLogI(APrefix + 'DocumentNumber = %s', [P.DocumentNumber]);
+  WriteLogI(APrefix + 'DocumentDate = %s', [P.DocumentDate]);
+  WriteLogI(APrefix + 'OperationCode = %s', [P.OperationCode]);
+  DumpAcceptanceInformation(P.AcceptanceInformation, APrefix + 'AcceptanceInformation');
+  DumpAdditionalInfoId4(P.AdditionalInfoId, APrefix + 'AdditionalInfoId');
+end;
+
+procedure TExchInfoFrame.DumpAcceptanceInformation(P: TAcceptanceInformation;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'OperationContent = %s', [P.OperationContent]);
+  WriteLogI(APrefix + 'AcceptanceDate = %s', [P.AcceptanceDate]);
+  DumpOperationCode(P.OperationCode, APrefix + 'OperationCode');
+  DumpCargoPerson(P.CargoPerson, APrefix + 'CargoPerson');
+end;
+
+procedure TExchInfoFrame.DumpOperationCode(P: TOperationCode; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'ItogCode = %s', [P.ItogCode]);
+  WriteLogI(APrefix + 'DocumentName = %s', [P.DocumentName]);
+  WriteLogI(APrefix + 'DocumentCode = %s', [P.DocumentCode]);
+  WriteLogI(APrefix + 'DocumentNumber = %s', [P.DocumentNumber]);
+  WriteLogI(APrefix + 'DocumentDate = %s', [P.DocumentDate]);
+  WriteLogI(APrefix + 'FileID = %s', [P.FileID]);
+end;
+
+procedure TExchInfoFrame.DumpCargoPerson(P: TCargoPerson; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  DumpCustomerEmployee(P.CustomerEmployee, APrefix + 'CustomerEmployee');
+  DumpExchangeOtherIssuer(P.OtherIssuer, APrefix + 'OtherIssuer');
+end;
+
+procedure TExchInfoFrame.DumpCustomerEmployee(P: TCustomerEmployee;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Position = %s', [P.Position]);
+  WriteLogI(APrefix + 'OtherInfo = %s', [P.OtherInfo]);
+  WriteLogI(APrefix + 'SignerPowersBase = %s', [P.SignerPowersBase]);
+  DumpExchangePerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TExchInfoFrame.DumpExchangePerson(P: TExchangePerson; APrefix: string
+  );
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Surname = %s', [P.Surname]);
+  WriteLogI(APrefix + 'FirstName = %s', [P.FirstName]);
+  WriteLogI(APrefix + 'Patronymic = %s', [P.Patronymic]);
+end;
+
+procedure TExchInfoFrame.DumpExchangeOtherIssuer(P: TExchangeOtherIssuer;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  DumpAacceptanceLegalEntityEmployee(P.LegalEntityEmployee, APrefix + 'LegalEntityEmployee');
+  DumpIndividualAcceptanceGoods(P.IndividualAcceptanceGoods, APrefix + 'IndividualAcceptanceGoods');
+end;
+
+procedure TExchInfoFrame.DumpAacceptanceLegalEntityEmployee(
+  P: TAacceptanceLegalEntityEmployee; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'Position = %s', [P.Position]);
+  WriteLogI(APrefix + 'OtherInfo = %s', [P.OtherInfo]);
+  WriteLogI(APrefix + 'Organization = %s', [P.Organization]);
+  WriteLogI(APrefix + 'OrgSignerPowersBase = %s', [P.OrgSignerPowersBase]);
+  WriteLogI(APrefix + 'SignerPowersBase = %s', [P.SignerPowersBase]);
+  DumpExchangePerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TExchInfoFrame.DumpIndividualAcceptanceGoods(
+  P: TIndividualAcceptanceGoods; APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'SignerPowersBase = %s', [P.SignerPowersBase]);
+  WriteLogI(APrefix + 'OtherInfo = %s', [P.OtherInfo]);
+  DumpExchangePerson(P.Person, APrefix + 'Person');
+end;
+
+procedure TExchInfoFrame.DumpAdditionalInfoId4(P: TAdditionalInfoId4;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+  WriteLogI(APrefix + 'FileID = %s', [P.FileID]);
+  DumpExchangeTextInfoList(P.TextInfo, APrefix + 'TextInfo');
+end;
+
+procedure TExchInfoFrame.DumpExchangeTextInfoList(P: TExchangeTextInfoList;
+  APrefix: string);
+var
+  i: Integer;
+begin
+  if not Assigned(P) then Exit;
+  for i:=0 to P.Count-1 do
+    DumpExchangeTextInfo(P[i], Format(APrefix + '[%d]', [i]));
+end;
+
+procedure TExchInfoFrame.DumpExchangeTextInfo(P: TExchangeTextInfo;
+  APrefix: string);
+begin
+  if not Assigned(P) then Exit;
+  APrefix:=APrefix + '.';
+
+  WriteLogI(APrefix + 'ID = %s', [P.ID]);
+  WriteLogI(APrefix + 'Value = %s', [P.Value]);
 end;
 
 procedure TExchInfoFrame.WriteLogI(S: string; AParams: array of const);
