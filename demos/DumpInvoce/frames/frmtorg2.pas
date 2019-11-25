8a252d3503b8f91ddbc18a16ed2e0b795e4755ab
lalexs75 <alexs75@yandex.ru>
2019-11-25 13:06:02 +0300

    Пример работы

--- demos/DumpInvoce/frames/frmtorg2.pas
+++ demos/DumpInvoce/frames/frmtorg2.pas
@@ -47,6 +47,23 @@ begin
   F.ParticipantsInformation.SellerExchangeInformation.IdentifierSenderOperator:='881';
 
   F.Document.KND:='1175014';
+  F.Document.DocumentDateCreate:='12.12.2019';
+  F.Document.DocumentTimeCreate:='12:12:12';
+  F.Document.DocumentNameEL:='Документ о приемке ценностей и (или) расхождениях, выявленных при их приемке';
+  F.Document.DocumentName:='Документ о приемке ценностей и (или) расхождениях или иное';
+  F.Document.DocumentCreator:='ООО Плюшка';
+  F.Document.DocumentCreatorBase:='Покупатель';
+  F.Document.DocumentCreateConditions:='1';
+  //F.Document.DocumentDestination:=
+  F.Document.AcceptanceDocumentDateNumber.DocumentNumber:='A0000123/12';
+  F.Document.AcceptanceDocumentDateNumber.DocumentDate:='01.01.2019';
+  F.Document.CorrectionDocumentDateNumber.CorrectionNumber:='1';
+  F.Document.CorrectionDocumentDateNumber.CorrectionDate:='01.02.2019';
+  //F.Document.AcceptanceInformation1:TAcceptanceInformation1 read FAcceptanceInformation1;
+  //F.Document.AcceptanceInformation2:TAcceptanceInformation2 read FAcceptanceInformation2;
+  //F.Document.AdditionalInformationState:string read FAdditionalInformationState write SetAdditionalInformationState;
+  //F.Document.Signer
+
 
   F.SaveToFile('/tmp/'+FN+'.xml');
   F.Free;
