023da4a74ef10adbc98b99b52c8e47728c632dd1
lalexs75 <alexs75@yandex.ru>
2020-07-10 13:11:24 +0300

    Копирайт в файле

--- crpt/km_cancellation.pas
+++ crpt/km_cancellation.pas
@@ -2,8 +2,6 @@
 
   Copyright (C) 2020 Lagunov Aleksey alexs75@yandex.ru
 
-  base on docs from http://api-docs.diadoc.ru
-
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
