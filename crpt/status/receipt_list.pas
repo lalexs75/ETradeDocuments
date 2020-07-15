cd5cc7684c0d547ae0193041b2ff20066a465fde
lalexs75 <alexs75@yandex.ru>
2020-07-15 08:46:42 +0300

    забыл файл

---
+++ crpt/status/receipt_list.pas
@@ -0,0 +1,308 @@
+{ GS1 interface library for FPC and Lazarus
+
+  Copyright (C) 2020 Lagunov Aleksey alexs75@yandex.ru
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+}
+unit receipt_list;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, JSONObjects, AbstractSerializationObjects;
+
+type
+
+  { TDocumentDataDto }
+
+  TDocumentDataDto = class(TJSONSerializationObject)
+  private
+    FErrors: TXSDStringArray;
+    procedure SetErrors(AValue: TXSDStringArray);
+  protected
+    procedure InternalRegisterPropertys; override;
+    procedure InternalInitChilds; override;
+  public
+    destructor Destroy; override;
+  published
+    property Errors:TXSDStringArray read FErrors write SetErrors;
+  end;
+
+  { TReceiptItem }
+
+  TReceiptItem = class(TJSONSerializationObject)
+  private
+    FAType: string;
+    FDocDate: string;
+    FDocErrors: TXSDStringArray;
+    FDocumentDataDto: TDocumentDataDto;
+    FDownloadDesc: string;
+    FDownloadStatus: string;
+    FErrors: TXSDStringArray;
+    FInput: Boolean;
+    FNumber: string;
+    FPdfFile: string;
+    FReceivedAt: string;
+    FSenderName: string;
+    FStatus: string;
+    FTotal: Cardinal;
+    procedure SetAType(AValue: string);
+    procedure SetDocDate(AValue: string);
+    procedure SetDocErrors(AValue: TXSDStringArray);
+    procedure SetDownloadDesc(AValue: string);
+    procedure SetDownloadStatus(AValue: string);
+    procedure SetErrors(AValue: TXSDStringArray);
+    procedure SetInput(AValue: Boolean);
+    procedure SetNumber(AValue: string);
+    procedure SetPdfFile(AValue: string);
+    procedure SetReceivedAt(AValue: string);
+    procedure SetSenderName(AValue: string);
+    procedure SetStatus(AValue: string);
+    procedure SetTotal(AValue: Cardinal);
+  protected
+    procedure InternalRegisterPropertys; override;
+    procedure InternalInitChilds; override;
+  public
+    destructor Destroy; override;
+  published
+    property Number:string read FNumber write SetNumber;
+    property DocDate:string read FDocDate write SetDocDate;
+    property ReceivedAt:string read FReceivedAt write SetReceivedAt;
+    property AType:string read FAType write SetAType;
+    property Status:string read FStatus write SetStatus;
+    property SenderName:string read FSenderName write SetSenderName;
+    property Total:Cardinal read FTotal write SetTotal;
+    property DownloadStatus:string read FDownloadStatus write SetDownloadStatus;
+    property DownloadDesc:string read FDownloadDesc write SetDownloadDesc;
+    property Input:Boolean read FInput write SetInput;
+    property PdfFile:string read FPdfFile write SetPdfFile;
+    property Errors:TXSDStringArray read FErrors write SetErrors;
+    property DocErrors:TXSDStringArray read FDocErrors write SetDocErrors;
+    property DocumentDataDto:TDocumentDataDto read FDocumentDataDto;
+  end;
+  TReceiptItemList = specialize GXMLSerializationObjectList<TReceiptItem>;
+
+  { TReceiptItems }
+
+  TReceiptItems = class(TJSONSerializationObject)
+  private
+    FResults: TReceiptItemList;
+    FTotal: Integer;
+    procedure SetTotal(AValue: Integer);
+  protected
+    procedure InternalRegisterPropertys; override;
+    procedure InternalInitChilds; override;
+  public
+    destructor Destroy; override;
+  published
+    property Results:TReceiptItemList read FResults;
+    property Total:Integer read FTotal write SetTotal;
+  end;
+
+
+implementation
+
+{ TDocumentDataDto }
+
+procedure TDocumentDataDto.SetErrors(AValue: TXSDStringArray);
+begin
+  if FErrors=AValue then Exit;
+  FErrors:=AValue;
+  ModifiedProperty('Errors');
+end;
+
+procedure TDocumentDataDto.InternalRegisterPropertys;
+begin
+  inherited InternalRegisterPropertys;
+  RegisterProperty('Errors', 'errors', [], '', -1, -1);
+end;
+
+procedure TDocumentDataDto.InternalInitChilds;
+begin
+  inherited InternalInitChilds;
+end;
+
+destructor TDocumentDataDto.Destroy;
+begin
+  inherited Destroy;
+end;
+
+{ TReceiptItem }
+
+procedure TReceiptItem.SetAType(AValue: string);
+begin
+  if FAType=AValue then Exit;
+  FAType:=AValue;
+  ModifiedProperty('AType');
+end;
+
+procedure TReceiptItem.SetDocDate(AValue: string);
+begin
+  if FDocDate=AValue then Exit;
+  FDocDate:=AValue;
+  ModifiedProperty('DocDate');
+end;
+
+procedure TReceiptItem.SetDocErrors(AValue: TXSDStringArray);
+begin
+  if FDocErrors=AValue then Exit;
+  FDocErrors:=AValue;
+  ModifiedProperty('DocErrors');
+end;
+
+procedure TReceiptItem.SetDownloadDesc(AValue: string);
+begin
+  if FDownloadDesc=AValue then Exit;
+  FDownloadDesc:=AValue;
+  ModifiedProperty('');
+end;
+
+procedure TReceiptItem.SetDownloadStatus(AValue: string);
+begin
+  if FDownloadStatus=AValue then Exit;
+  FDownloadStatus:=AValue;
+  ModifiedProperty('DownloadStatus');
+end;
+
+procedure TReceiptItem.SetErrors(AValue: TXSDStringArray);
+begin
+  if FErrors=AValue then Exit;
+  FErrors:=AValue;
+  ModifiedProperty('Errors');
+end;
+
+procedure TReceiptItem.SetInput(AValue: Boolean);
+begin
+  if FInput=AValue then Exit;
+  FInput:=AValue;
+  ModifiedProperty('Input');
+end;
+
+procedure TReceiptItem.SetNumber(AValue: string);
+begin
+  if FNumber=AValue then Exit;
+  FNumber:=AValue;
+  ModifiedProperty('Number');
+end;
+
+procedure TReceiptItem.SetPdfFile(AValue: string);
+begin
+  if FPdfFile=AValue then Exit;
+  FPdfFile:=AValue;
+  ModifiedProperty('PdfFile');
+end;
+
+procedure TReceiptItem.SetReceivedAt(AValue: string);
+begin
+  if FReceivedAt=AValue then Exit;
+  FReceivedAt:=AValue;
+  ModifiedProperty('ReceivedAt');
+end;
+
+procedure TReceiptItem.SetSenderName(AValue: string);
+begin
+  if FSenderName=AValue then Exit;
+  FSenderName:=AValue;
+  ModifiedProperty('SenderName');
+end;
+
+procedure TReceiptItem.SetStatus(AValue: string);
+begin
+  if FStatus=AValue then Exit;
+  FStatus:=AValue;
+  ModifiedProperty('Status');
+end;
+
+procedure TReceiptItem.SetTotal(AValue: Cardinal);
+begin
+  if FTotal=AValue then Exit;
+  FTotal:=AValue;
+  ModifiedProperty('Total');
+end;
+
+procedure TReceiptItem.InternalRegisterPropertys;
+begin
+  inherited InternalRegisterPropertys;
+  RegisterProperty('Number', 'number', [], '', -1, -1);
+  RegisterProperty('DocDate', 'docDate', [], '', -1, -1);
+  RegisterProperty('ReceivedAt', 'receivedAt', [], '', -1, -1);
+  RegisterProperty('AType', 'type', [], '', -1, -1);
+  RegisterProperty('Status', 'status', [], '', -1, -1);
+  RegisterProperty('SenderName', 'senderName', [], '', -1, -1);
+  RegisterProperty('Total', 'total', [], '', -1, -1);
+  RegisterProperty('DownloadStatus', 'downloadStatus', [], '', -1, -1);
+  RegisterProperty('DownloadDesc', 'downloadDesc', [], '', -1, -1);
+  RegisterProperty('Input', 'input', [], '', -1, -1);
+  RegisterProperty('PdfFile', 'pdfFile', [], '', -1, -1);
+  RegisterProperty('DocErrors', 'docErrors', [], '', -1, -1);
+  RegisterProperty('Errors', 'errors', [], '', -1, -1);
+  RegisterProperty('DocumentDataDto', 'documentDataDto', [], '', -1, -1);
+end;
+
+procedure TReceiptItem.InternalInitChilds;
+begin
+  inherited InternalInitChilds;
+  FDocumentDataDto:=TDocumentDataDto.Create;
+end;
+
+destructor TReceiptItem.Destroy;
+begin
+  FreeAndNil(FDocumentDataDto);
+  inherited Destroy;
+end;
+
+{ TReceiptItems }
+
+procedure TReceiptItems.SetTotal(AValue: Integer);
+begin
+  if FTotal=AValue then Exit;
+  FTotal:=AValue;
+  ModifiedProperty('Total');
+end;
+
+procedure TReceiptItems.InternalRegisterPropertys;
+begin
+  inherited InternalRegisterPropertys;
+  RegisterProperty('Results', 'results', [], '', -1, -1);
+  RegisterProperty('Total', 'total', [], '', -1, -1);
+end;
+
+procedure TReceiptItems.InternalInitChilds;
+begin
+  inherited InternalInitChilds;
+  FResults:=TReceiptItemList.Create;
+end;
+
+destructor TReceiptItems.Destroy;
+begin
+  FreeAndNil(FResults);
+  inherited Destroy;
+end;
+
+end.
+
